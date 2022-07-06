b298e8c66290209b8f69d8a05e6800b498cad09a
aboladelawal <aboladelawal@gmail.com>
2022-07-06 08:02:31 -0700

    new commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - main block of code to be executed 
+* Description: computes and print sum of all multiples of 3
+* Return: 0
+*/
+int main(void)
+{
+int c = 0;
+int sum = 0;
+while (c < 1024)
+{
+if (c % 3 == 0 || c % 5 == 0)
+{
+sum += c;
+}
+c++;
+}
+printf("%i\n", sum);
+return (0);
+}
