5ecd424537cfcb288745394e997ba679aa36ffac
aboladelawal <aboladelawal@gmail.com>
2022-08-02 14:01:16 -0700

    update 5-free

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 /**
 * free_dog - Frees dogs.
-* @d: The dog to be freed.
+* @d:pointer to The dog to be freed.
+* Return: void.
 */
 void free_dog(dog_t *d)
 {
-if (d == NULL)
+if (d == 0)
 return;
 free(d->owner);
 free(d->name);
